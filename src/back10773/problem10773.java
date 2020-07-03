e98ce44d906d2fd48a285c3e5381b3f8b456361b
Sim sungyong <ssy332972@gmail.com>
2020-07-03 16:32:20 +0900

    s

--- src/back10773/problem10773.java
+++ src/back10773/problem10773.java
@@ -1,11 +1,12 @@
 package back10773;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class problem10773 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        int n = Integer.parseInt(br.readLine());
     }
 }
